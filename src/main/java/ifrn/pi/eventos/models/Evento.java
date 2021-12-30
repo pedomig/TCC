77ca1f63841d9f90450a55fc67f4cf50f3772668
rudysondmo <rudysondmo@321>
2021-12-30 16:27:05 -0300

    Criando o boletim no bd

--- src/main/java/ifrn/pi/eventos/models/Evento.java
+++ src/main/java/ifrn/pi/eventos/models/Evento.java
@@ -20,59 +20,99 @@ public class Evento {
 	private Long id;
 	
 	@NotBlank
-	private String nome;
+	private String cidade;
 	@NotBlank
-	private String local;
+	private String Cep;
+	@NotBlank
+	private String PDose;
+	@NotBlank
+	private String SDose;
+	
+	@NotBlank
+	private String casos;
+	@NotBlank
+	private String recuperados;
+	@NotBlank
+	private String observacao;
+	
+	
 	@NotNull
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate data;
 	@NotNull
 	private LocalTime horario;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getLocal() {
-		return local;
-	}
-
-	public void setLocal(String local) {
-		this.local = local;
-	}
-
-	public LocalDate  getData() {
-		return data;
-	}
-
-	public void setData(LocalDate data) {
-		this.data = data;
-	}
-
-	public LocalTime getHorario() {
-		return horario;
-	}
-
-	public void setHorario(LocalTime horario) {
-		this.horario = horario;
-	}
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getCep() {
+		return Cep;
+	}
+	public void setCep(String cep) {
+		Cep = cep;
+	}
+	public String getPDose() {
+		return PDose;
+	}
+	public void setPDose(String pDose) {
+		PDose = pDose;
+	}
+	public String getSDose() {
+		return SDose;
+	}
+	public void setSDose(String sDose) {
+		SDose = sDose;
+	}
+	public String getCasos() {
+		return casos;
+	}
+	public void setCasos(String casos) {
+		this.casos = casos;
+	}
+	public String getRecuperados() {
+		return recuperados;
+	}
+	public void setRecuperados(String recuperados) {
+		this.recuperados = recuperados;
+	}
+	public String getObservacao() {
+		return observacao;
+	}
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	public LocalTime getHorario() {
+		return horario;
+	}
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
 	@Override
 	public String toString() {
-		return "Evento [id=" + id + ", nome=" + nome + ", local=" + local + ", data=" + data + ", horario=" + horario
-				+ "]";
+		return "Evento [id=" + id + ", cidade=" + cidade + ", Cep=" + Cep + ", PDose=" + PDose + ", SDose=" + SDose
+				+ ", casos=" + casos + ", recuperados=" + recuperados + ", observacao=" + observacao + ", data=" + data
+				+ ", horario=" + horario + "]";
 	}
+	
+	
+	
+
+	
+	
 
 
 }
