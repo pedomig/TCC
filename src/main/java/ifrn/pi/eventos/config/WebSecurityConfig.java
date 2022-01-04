503d42b518d9a224ea0c6414b9a6aa0ac98284be
rudysondmo <rudysondmo@321>
2022-01-04 15:13:43 -0300

    concertando  o login inicial

--- src/main/java/ifrn/pi/eventos/config/WebSecurityConfig.java
+++ src/main/java/ifrn/pi/eventos/config/WebSecurityConfig.java
@@ -10,7 +10,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/form").hasRole("ADMGERAL")
+			.antMatchers("/css's/**", "/imagens/**").permitAll()
+			.antMatchers("/form").hasRole("ADMGERAL")
 				.antMatchers("/form").hasRole("ADMLOCAL")
 				.anyRequest()
 				.authenticated()
